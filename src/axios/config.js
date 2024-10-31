5936e81c6e56cdc2e725250097beec7575add00f
WanessaT <wanessatlsousa@gmail.com>
2024-10-31 15:47:29 -0300

    feat: incluindo novo endereço da API

--- src/axios/config.js
+++ src/axios/config.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const blogFetch = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://blog-api-y04r.onrender.com",
     headers: {
         "Content-Type": "application/json",
     }
